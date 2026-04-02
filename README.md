ca6d13f4c37e3491568193cf8e1a60a67044d16a
1LyyAA <ilyaiudov678@gmail.com>
2026-04-03 01:46:32 +0300

    add cronjob, finish deploy.sh and readme

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-![alt text](pics/image.png)
-
+### Проверка баланса нагрузки
 запросы делал из
 kubectl run -it --rm debug-pod --image=alpine --restart=Never -- sh
 
+![alt text](pics/image.png)
 
-проверка логгера
+
+
+
+## проверка логгера
 ![alt text](pics/image-1.png)
+
+
+Для развёртывания:
+
+```bash
+bash deploy.sh
--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,25 @@
 
-
-
-
-minikube start
+# minikube start
 
 docker build -t example-app:latest .
 minikube image load example-app:latest
 
-kubectl apply -f pod.yaml
+# kubectl apply -f pod.yaml
 
-kubectl apply -f configmap.yaml
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
-kubectl apply -f daemonset.yaml
+kubectl apply -f k8s/configmap.yaml
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
+kubectl apply -f k8s/daemonset.yaml
+kubectl apply -f k8s/cronjob.yaml
 
-minikube stop
+
+
+echo "Waiting for Deployment to be ready..."
+kubectl rollout status deployment/app-deployment
+
+echo "Waiting for DaemonSet to be ready..."
+kubectl rollout status daemonset/log-agent
+
+echo "=== Deployment finished successfully ==="
+
+# minikube stop
